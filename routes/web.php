2e12fb5e1a96a7ca8b725bd330509afc554b259c
Andre Amorim <aamorim@localhost.localdomain>
2021-07-20 19:10:41 -0300

    metrics for php-fpm

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,48 @@ Route::get('/', function () {
 
 Route::get('/metrics', function () {
 
+    $clientGuzz = new \GuzzleHttp\Client();
+
+    $response = $clientGuzz->request('GET', "http://localhost:8080/fpm-status?json");
+    $content = json_decode(str_replace(" ", "", $response->getBody()));
+
+
     $adapter = new APC();
     $registry = new CollectorRegistry($adapter);
     $renderer = new RenderTextFormat();
+
+    $gauge = $registry->getOrRegisterGauge('php_fpm', 'accepted_connections_total', '', ['type']);
+    $gauge->set($content->acceptedconn, ['php-fpm']);
+
+    $gauge = $registry->getOrRegisterGauge('php_fpm', 'active_processes', '', ['type']);
+    $gauge->set($content->activeprocesses, ['php-fpm']);
+
+    $gauge = $registry->getOrRegisterGauge('php_fpm', 'idle_processes', '', ['type']);
+    $gauge->set($content->idleprocesses, ['php-fpm']);
+
+    $gauge = $registry->getOrRegisterGauge('php_fpm', 'listen_queue', '', ['type']);
+    $gauge->set($content->listenqueue, ['php-fpm']);
+
+    $gauge = $registry->getOrRegisterGauge('php_fpm', 'listen_queue_length', '', ['type']);
+    $gauge->set($content->listenqueuelen, ['php-fpm']);
+
+    $gauge = $registry->getOrRegisterGauge('php_fpm', 'max_active_processes', '', ['type']);
+    $gauge->set($content->maxactiveprocesses, ['php-fpm']);
+
+    $gauge = $registry->getOrRegisterGauge('php_fpm', 'max_children_reached', '', ['type']);
+    $gauge->set($content->maxchildrenreached, ['php-fpm']);
+
+    $gauge = $registry->getOrRegisterGauge('php_fpm', 'max_listen_queue', '', ['type']);
+    $gauge->set($content->maxlistenqueue, ['php-fpm']);
+
+    $gauge = $registry->getOrRegisterGauge('php_fpm', 'slow_requests', '', ['type']);
+    $gauge->set($content->slowrequests, ['php-fpm']);
+
+    $gauge = $registry->getOrRegisterGauge('php_fpm', 'start_time_seconds', '', ['type']);
+    $gauge->set($content->starttime, ['php-fpm']);
+
+    $gauge = $registry->getOrRegisterGauge('php_fpm', 'total_processes', '', ['type']);
+    $gauge->set($content->totalprocesses, ['php-fpm']);
 
     $result = $renderer->render($registry->getMetricFamilySamples());
 
